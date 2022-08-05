8bf91a6aedcbcf31ddcb4afee315fba32319e568
sieun <supiya0908@naver.com>
2022-08-05 15:49:03 +0900

    SPRING BASIC-JPA & AOP

--- src/main/java/sieun/hellospring/SpringConfig.java
+++ src/main/java/sieun/hellospring/SpringConfig.java
@@ -3,33 +3,60 @@ package sieun.hellospring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import sieun.hellospring.repository.JdbcMemberRepository;
-import sieun.hellospring.repository.JdbcTemplateMemberRepository;
-import sieun.hellospring.repository.MemberRepository;
-import sieun.hellospring.repository.MemoryMemberRepository;
+import sieun.hellospring.aop.TimeTraceAop;
+import sieun.hellospring.repository.*;
 import sieun.hellospring.service.MemberService;
 
-import javax.sql.DataSource;
+import java.sql.Time;
 
 @Configuration
 public class SpringConfig {
+    /**
+     * JDBC
+     */
+    // private DataSource dataSource;
 
-    private DataSource dataSource;
+    // @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+
+    /**
+     * JPA
+      */
+//    @PersistenceContext
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+
+    /**
+     * Data JPA
+     */
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
+
+    //@Bean
+    //public MemberRepository memberRepository() {
         // return new MemoryMemberRepository();
         // return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
-    }
+        // return new JdbcTemplateMemberRepository(dataSource);
+        //return new JpaMemberRepository(em);
+    //}
 }
--- src/main/java/sieun/hellospring/service/MemberService.java
+++ src/main/java/sieun/hellospring/service/MemberService.java
@@ -2,6 +2,7 @@ package sieun.hellospring.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sieun.hellospring.domain.Member;
 import sieun.hellospring.repository.MemberRepository;
 import sieun.hellospring.repository.MemoryMemberRepository;
@@ -9,6 +10,7 @@ import sieun.hellospring.repository.MemoryMemberRepository;
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
